ad5a6849f97eb6a0c3801de01a9cddc792ccb448
Nat <95653800+natcat256@users.noreply.github.com>
2023-01-02 18:42:59 -0500

    Fix parameter name in header definition

--- Monopoly/Injector.h
+++ Monopoly/Injector.h
@@ -21,6 +21,6 @@ namespace Monopoly
 		Injector(String^ processName);
 
 		void Inject(array<byte>^ assemblyBytes, String^ __identifier(namespace), String^ __identifier(class), String^ method);
-		void Inject(String^ assemblyName, String^ __identifier(namespace), String^ __identifier(class), String^ method);
+		void Inject(String^ assemblyPath, String^ __identifier(namespace), String^ __identifier(class), String^ method);
 	};
 }
